2071b4d15711dfb86f944333f37fc0ba9559cb0c
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2019-05-26 00:06:37 -0500

    commit

--- app/src/main/java/cursos/omar/com/MainActivity.java
+++ app/src/main/java/cursos/omar/com/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Comentario
+
 
 
 
